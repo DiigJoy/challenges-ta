c1fa108e0533e0429192a7e793f8b4837a664120
Jorge Barrios <jlbarriossouza@gmail.com>
2025-03-23 20:52:27 -0300

    cambio sin .env

--- servicio-2/servicio_2.py
+++ servicio-2/servicio_2.py
@@ -4,7 +4,6 @@ from influxdb_client import InfluxDBClient, Point
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 # Configuración MQTT
 BROKER = "mosquitto"
@@ -12,10 +11,11 @@ PORT = 1883
 TOPIC = "challenge/dispositivo/rx"
 
 # Configuración InfluxDB
-INFLUX_URL = os.getenv("INFLUX_URL")
-INFLUX_TOKEN = os.getenv("INFLUX_TOKEN")
-INFLUX_ORG = os.getenv("INFLUX_ORG")
-INFLUX_BUCKET = os.getenv("INFLUX_BUCKET")
+INFLUX_URL = "http://influx:8086"
+INFLUX_TOKEN = "_Gcd7AQSMFyOpbINETVW1DPW7Zi0rQCsX2SMwfX7BctoHJ7der7cm4H1VNAORWN0c4DdkpIPFQJk_ntqvxqsPw=="
+INFLUX_ORG = "tecnoandina"
+INFLUX_BUCKET = "system"
+
 
 # Inicializar InfluxDB client
 influx_client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
--- servicio-3/service/influx_service.py
+++ servicio-3/service/influx_service.py
@@ -3,13 +3,12 @@ from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 # Configuración InfluxDB
-INFLUX_URL = os.getenv("INFLUX_URL")
-INFLUX_TOKEN = os.getenv("INFLUX_TOKEN")
-INFLUX_ORG = os.getenv("INFLUX_ORG")
-INFLUX_BUCKET = os.getenv("INFLUX_BUCKET")
+INFLUX_URL = "http://influx:8086"
+INFLUX_TOKEN = "_Gcd7AQSMFyOpbINETVW1DPW7Zi0rQCsX2SMwfX7BctoHJ7der7cm4H1VNAORWN0c4DdkpIPFQJk_ntqvxqsPw=="
+INFLUX_ORG = "tecnoandina"
+INFLUX_BUCKET = "system"
 
 client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
 query_api = client.query_api()
