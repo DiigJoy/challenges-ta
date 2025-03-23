1341522d9a20b2a020a35428298dbbc939e75a27
Jorge Barrios <jlbarriossouza@gmail.com>
2025-03-23 20:20:13 -0300

    adicion de cryptography en requirements y cambio en mysql

--- servicio-3/service/mysql_service.py
+++ servicio-3/service/mysql_service.py
@@ -6,7 +6,13 @@ import os
 
 load_dotenv()
 
-DB_URL = os.getenv("DB_URL")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "3306")  # Por si no lo ponen
+DB_NAME = os.getenv("DB_NAME")
+
+DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(bind=engine)
