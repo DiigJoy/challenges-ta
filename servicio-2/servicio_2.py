66a471ff5a832e664c7a11d7d179085b406a0255
Jorge Barrios <jorge.barrios_ext@casaideas.com>
2025-03-26 21:31:42 -0300

    cambios

--- servicio-2/servicio_2.py
+++ servicio-2/servicio_2.py
@@ -10,7 +10,7 @@ TOPIC = "challenge/dispositivo/rx"
 
 # Configuración InfluxDB
 INFLUX_URL = "http://influx:8086"
-INFLUX_TOKEN = "_Gcd7AQSMFyOpbINETVW1DPW7Zi0rQCsX2SMwfX7BctoHJ7der7cm4H1VNAORWN0c4DdkpIPFQJk_ntqvxqsPw=="
+INFLUX_TOKEN = "fJzWkx2IhKIVdasCuRzv9p7f-9EL8oa8GO_CuF3JdVtArTRIrn07mzcvwlxMYDYg7AEtnvw4te38fNYSmndkTg=="
 INFLUX_ORG = "tecnoandina"
 INFLUX_BUCKET = "system"
 
